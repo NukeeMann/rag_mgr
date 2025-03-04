2084baf339bf8afcbdea5bfb021e4ea148c9b05b
Michal <nukeemann@gmail.com>
2025-03-04 10:23:47 +0100

    Update chat template

--- notebooks/scripts/rag.py
+++ notebooks/scripts/rag.py
@@ -372,7 +372,7 @@ class RAG:
     def apply_template(self, query, documents, additional_instruct, use_rag):
         messages = []
         if use_rag:
-            if self.gen_model!='':#'speakleash/Bielik-11B-v2.3-Instruct':
+            if self.gen_model=='speakleash/Bielik-11B-v2.3-Instruct':
                 messages.append({"role": "system", "content": f"Na podstawie dostarczonych poniżej dokumentów odpowiedz na pytanie użytkownika które znajduję się na samym dole. Wnioskuj wyłącznie na podstawie dostarczonego kontekstu. Jeżeli nie jesteś w stanie odpowiedzieć na podstawie otrzymanych dokumentów uczciwie to powiedz."})
                 for id, doc in enumerate(documents):
                     messages.append({"role": "system", "content": f"Dokument {id}: {doc['_source'].get('source_text')}"})
@@ -380,15 +380,26 @@ class RAG:
                 if additional_instruct:
                     messages.append({"role": "system", "content": additional_instruct})
             else:
-                assistant_msg = f"{query['source_text']} . Odpowiedz na pytanie użytkownika. Poniżej jako kontekst możesz wykorzystać dostarczone powiązane z pytaniem dokumenty w które mogą pomóc Ci poprawnie odpowiedzieć. {additional_instruct}. "
-                for id, doc in enumerate(documents):
-                    assistant_msg += f"\n Dokument {id}: {doc['_source'].get('source_text')} "
+                assistant_msg = f'''
+                Odpowiedz na pytanie użytkownika.
+                Poniżej jako kontekst możesz wykorzystać dostarczone powiązane z pytaniem dokumenty w które mogą pomóc Ci poprawnie odpowiedzieć.
+                {additional_instruct}
+                
+                Pytanie użytkownika:
+                {query['source_text']}
+
+                Powiązane dokumenty:
+                {documents}
+                '''
+                # assistant_msg = f"{query['source_text']} .  {additional_instruct}. "
+                # for id, doc in enumerate(documents):
+                #     assistant_msg += f"\n Dokument {id}: {doc['_source'].get('source_text')} "
                 
                 messages.append({"role": "user", "content": assistant_msg})
         else:
             messages.append({"role": "system", "content": f"Odpowiedz na pytanie użytkownika. {additional_instruct}"})
         
-        if self.gen_model!='':#'speakleash/Bielik-11B-v2.3-Instruct':
+        if self.gen_model=='speakleash/Bielik-11B-v2.3-Instruct':
             messages.append({"role": "user", "content": f"Odpowiedz na poniższe pytanie: {query['source_text']}"})
 
         return messages
